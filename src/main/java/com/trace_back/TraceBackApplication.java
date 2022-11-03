8bbd19b6b7e38876b5baded0919c9b702831a750
Aukira <lixinran1826@163.com>
2022-11-03 21:56:41 +0800

    init Springboot project

---
+++ src/main/java/com/trace_back/TraceBackApplication.java
@@ -0,0 +1,13 @@
+package com.trace_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trace_back/TraceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trace_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
